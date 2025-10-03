9be336aafe08553d862f1ac93c6a6f1f3aa62196
Fida-Ukwishaka <fidaukwishaka@gmail.com>
2025-10-03 13:33:25 +0000

    Create tryout.java

---
+++ tryout.java
@@ -0,0 +1,10 @@
+public class tryout {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 35;
+        int sum = num1+num2;
+        sum -= 1;
+        System.out.println(sum--);
+
+    }
+}
